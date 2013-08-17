175115acf29e5a2f735b7d1c0edb9ca97aa106ab
Martin Ring <martinring@live.de>
2013-08-17 12:22:50 +0200

    fixed vertical centering bug

--- app/models/Folder.scala
+++ app/models/Folder.scala
@@ -3,8 +3,8 @@ package models
 import scala.slick.driver.H2Driver.simple._
 import Database.{threadLocalSession => session}
 
-case class Folder(id: Long, name: String, )
+case class Folder(id: Long, name: String)
 
-object Folders extends Table[Folder]("folders") {
+object Folders /* extends Table[Folder]("folders") {
   
-}
+}*/
