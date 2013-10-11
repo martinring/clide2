2b1f401b8cb32aa28a63a01d6e8ecc6c6b1fb4d0
Martin Ring <martinring@live.de>
2013-10-11 12:19:40 +0200

    using xml tree for markup

--- modules/clide-isabelle/src/main/scala/clide/plugins/IsabelleMarkup.scala
+++ modules/clide-isabelle/src/main/scala/clide/plugins/IsabelleMarkup.scala
@@ -2,25 +2,38 @@ package clide.plugins
 
 import isabelle._
 import scala.collection.SortedMap
+import clide.collaboration.{Annotation,Plain,Annotate}
 
-object IsabelleMarkup {
-  import Markup._
-  
-  val inner = Set(TVAR,FREE,SORT,TYP,TERM,PROP,ML_TYPING,TOKEN_RANGE,ENTITY,
-      KEYWORD, COMMAND, TYPING,FREE,SKOLEM,BOUND,VAR,TFREE,TVAR,ML_SOURCE,
-      DOCUMENT_SOURCE, ML_KEYWORD)
-  
-  def getTokens(snapshot: Document.Snapshot,range: Text.Range) = {                
-    snapshot.cumulate_markup[List[String]](
-      range,
-      Nil,
-      Some(inner),
-      _ => { case (x, m) => List(m.info.markup.name) })   
+object IsabelleMarkup {  
+  def annotations(xml: XML.Tree, c: Option[Map[String,String]] = None): List[Annotation] = xml match {
+    case XML.Wrapped_Elem(markup, body, body2) =>
+      println("markup: " + markup)
+      println("body:   " + body)
+      // TODO: use body
+      body2.flatMap(annotations(_))
+    case XML.Elem(markup, body) =>
+      val c2 = markup.name match {
+        case Markup.COMMAND => Some(Map("c"->"command"))
+        case Markup.KEYWORD  | 
+             Markup.KEYWORD1 | 
+             Markup.KEYWORD2 => Some(Map("c" -> "keyword"))
+        case Markup.STRING  => Some(Map("c" -> "string"))
+        case Markup.COMMENT => Some(Map("c" -> "comment"))
+        case Markup.BAD | Markup.ERROR => Some(Map("c" -> "error"))
+        case Markup.WARNING => Some(Map("c" -> "warning"))
+        case Markup.ENTITY  => Some(Map("c" -> "entity"))
+        case _              => None
+      }
+      val n = c.flatMap(c => c2.map(c ++ _)).orElse(c).orElse(c2)      
+      body.flatMap(annotations(_,n))
+    case XML.Text(content)      => c match {
+        case None    => List(Plain(content.length))
+        case Some(c) => List(Annotate(content.length,c))
+      }      
   }
-  
-  def getMessages(snapshot: Document.Snapshot,range: Text.Range) = {
-    snapshot.cumulate_markup[List[(String,String)]](
-      range,Nil,Some(Set(ERROR,WARNING,BAD)),
-      _ => { case (x, m) => List("e" -> m.info.toString) })
+      
+  def annotations(snapshot: Document.Snapshot): List[Annotation] = {
+    val xml = snapshot.state.markup_to_XML(snapshot.version, snapshot.node, _ => true)
+    xml.flatMap(annotations(_))
   }
 }
