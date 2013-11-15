6cc301363b2b197f465d73fed1370e22c83a9c29
Martin Ring <mail@martinring.de>
2013-11-15 18:13:34 +0100

    build settings

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,21 @@
  **       / __| | |/ _` |/ _ \     (c) 2012-2013 Martin Ring                  **
  **      | (__| | | (_| |  __/     http://clide.flatmap.net                   **
  **       \___|_|_|\__,_|\___|                                                **
+ **                                                                           **
+ **  This file is part of Clide.                                              **
+ **                                                                           **
+ **  Clide is free software: you can redistribute it and/or modify            **
+ **  it under the terms of the GNU General Public License as published by     **
+ **  the Free Software Foundation, either version 3 of the License, or        **
+ **  (at your option) any later version.                                      **
+ **                                                                           **
+ **  Clide is distributed in the hope that it will be useful,                 **
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of           **
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            **
+ **  GNU General Public License for more details.                             **
+ **                                                                           **
+ **  You should have received a copy of the GNU General Public License        **
+ **  along with Clide.  If not, see <http://www.gnu.org/licenses/>.           **
  \*                                                                           */
 
 import sbt._
@@ -12,7 +27,9 @@ object Dependencies {
   val slick = "com.typesafe.slick" %% "slick" % "1.0.1"
   val h2    = "com.h2database" % "h2" % "1.3.166"
   val slf4j = "org.slf4j" % "slf4j-nop" % "1.6.4"
-  
+
+  val jscala = "org.jscala" %% "jscala-macros" % "0.3-SNAPSHOT"
+
   val scalatest  = "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"
 
@@ -28,8 +45,8 @@ object Dependencies {
     val resolver = "spray" at "http://repo.spray.io/"
     val json     = "io.spray" %% "spray-json" % "1.2.5"
   }
-  
-  object scala {    
+
+  object scala {
     val version  = "2.10.3"
     val swing    = "org.scala-lang" % "scala-swing"   % version
     val actors   = "org.scala-lang" % "scala-actors"  % version
