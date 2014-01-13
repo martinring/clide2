af7e1253b649b95872b4c283523775f4097f137d
Martin Ring <mail@martinring.de>
2014-01-13 09:58:12 +0100

    add transformation to operation formalization

--- modules/clide-collaboration/src/main/isabelle/clide/collaboration/Operation.thy
+++ modules/clide-collaboration/src/main/isabelle/clide/collaboration/Operation.thy
@@ -41,10 +41,73 @@ where
 | "applyOp (Insert s#next) (doc) = append s (applyOp next doc)"
 | "applyOp (Delete n#next) (doc) = applyOp next (drop n doc)"
 
-lemma applyOpEq: "(a,b) \<in> operations \<Longrightarrow> SimpleOperation.applyOp a d = Some d' \<Longrightarrow> applyOp b d = d'" 
+lemma applyOpEq: "(a,b) \<in> operations \<Longrightarrow> \<forall> d'. SimpleOperation.applyOp a d = Some d' \<longrightarrow> applyOp b d = d'" 
+  apply (rule operations.induct)
+    apply (simp)    
   sorry
 
-(*fun compose :: "'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation"
-  "compose *)
+fun addRetain :: "nat \<Rightarrow> 'char operation \<Rightarrow> 'char operation" where
+  "addRetain n (Retain m#xs) = (Retain (n + m))#xs"
+| "addRetain n xs            = (Retain n)#xs"
+
+fun addInsert :: "'char list \<Rightarrow> 'char operation \<Rightarrow> 'char operation" where
+  "addInsert s (Delete d#xs) = (Delete d)#addInsert s xs"
+| "addInsert s (Insert t#xs) = (Insert (append t s))#xs"
+| "addInsert s xs            = (Insert s)#xs"
+
+fun addDelete :: "nat \<Rightarrow> 'char operation \<Rightarrow> 'char operation" where
+  "addDelete n (Delete m#xs) = (Delete (n + m))#xs"
+| "addDelete n xs            = (Delete n)#xs"
+
+fun composeRec :: "'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation" where
+  "composeRec [] [] xs  = xs" 
+| "composeRec ((Delete n)#as) (bb) xs = composeRec as bb (addDelete n xs)"
+| "composeRec (aa) ((Insert s)#bs) xs = composeRec aa bs (addInsert s xs)"
+| "composeRec ((Retain n)#as) ((Retain m)#bs) xs =
+   (if      (n < m) then composeRec as (Retain(m - n)#bs) (addRetain n xs)
+    else if (n = m) then composeRec as bs (addRetain n xs)
+    else                 composeRec (Retain(n - m)#as) bs (addRetain n xs))"
+| "composeRec ((Retain n)#as) ((Delete m)#bs) xs =
+   (if      (n < m) then composeRec as (Delete(m - n)#bs) (addDelete n xs)
+    else if (n = m) then composeRec as bs (addDelete n xs)
+    else                 composeRec (Retain(n - m)#as) bs (addDelete n xs))"
+| "composeRec ((Insert s)#as) ((Retain m)#bs) xs =
+   (if      (length s < m) then composeRec as (Delete(m - length s)#bs) (addInsert s xs)
+    else if (length s = m) then composeRec as bs (addInsert s xs)
+    else                        composeRec (Insert(drop m s)#as) bs (addInsert (take m s) xs))"
+| "composeRec ((Insert s)#as) ((Delete m)#bs) xs =
+   (if      (length s < m) then composeRec as (Delete(m - length s)#bs) xs
+    else if (length s = m) then composeRec as bs xs
+    else                        composeRec (Insert(drop m s)#as) bs xs)"
+
+definition compose :: "'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation" where
+  "compose a b = composeRec a b []"
+
+lemma composeEq: "(a, b) \<in> operations \<Longrightarrow> (a', b') \<in> operations \<Longrightarrow> SimpleOperation.compose a a' = Some a'' \<Longrightarrow> (a'', Operation.compose b b') \<in> operations"
+  sorry
+
+fun transformRec :: "'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation \<Rightarrow> 'char operation \<times> 'char operation" where
+  "transformRec [] [] xs ys = (xs,ys)"
+| "transformRec (Insert s#as) bs xs ys = transformRec as bs (addInsert s xs) (addRetain (length s) ys)"
+| "transformRec as (Insert s#bs) xs ys = transformRec as bs (addRetain (length s) xs) (addInsert s ys)"
+| "transformRec (Retain n#as) (Retain m#bs) xs ys = 
+   (if      (n < m) then transformRec as ((Retain (m - n))#bs) (addRetain n xs) (addRetain n ys)
+    else if (n = m) then transformRec as bs (addRetain n xs) (addRetain n ys)
+    else                 transformRec ((Retain (n - m))#as) bs (addRetain m xs) (addRetain m ys))"
+| "transformRec (Delete n#as) (Delete m#bs) xs ys = 
+   (if      (n < m) then transformRec as ((Delete (m - n))#bs) xs ys
+    else if (n = m) then transformRec as bs xs ys
+    else                 transformRec ((Delete (n - m))#as) bs xs ys)"
+| "transformRec (Retain n#as) (Delete m#bs) xs ys = 
+   (if      (n < m) then transformRec as ((Delete (m - n))#bs) xs (addDelete n ys)
+    else if (n = m) then transformRec as bs xs (addDelete m ys)
+    else                 transformRec ((Retain (n - m))#as) bs xs (addDelete m ys))"
+| "transformRec (Delete n#as) (Retain m#bs) xs ys = 
+   (if      (n < m) then transformRec as ((Retain (m - n))#bs) (addDelete n xs) ys
+    else if (n = m) then transformRec as bs (addDelete n xs) ys
+    else                 transformRec ((Delete (n - m))#as) bs (addDelete m xs) ys)"
+
+definition transform :: "'char operation \<Rightarrow> 'char operation \<Rightarrow> ('char operation \<times> 'char operation)" where
+  "transform a b = transformRec a b [] []"
 
 end
