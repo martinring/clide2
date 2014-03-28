d603c27fb6e07ee5cdea40cf9768b4275ca2965c
Martin Ring <mail@martinring.de>
2014-03-28 10:27:06 +0100

    minimal working sample

--- modules/clide-client/src/main/scala/clide/client/App.scala
+++ modules/clide-client/src/main/scala/clide/client/App.scala
@@ -1,6 +1,5 @@
 package clide.client
 
-import clide.reactive.ui._
 import clide.reactive.ui.html.HTML5
 import scala.scalajs.js.annotation.JSExport
 import clide.reactive.Event
@@ -9,19 +8,16 @@ import scala.language.postfixOps
 import scala.language.reflectiveCalls
 import scala.language.existentials
 import org.scalajs.dom
+import clide.xml._
+import clide.reactive.ui.InsertionContext
 
 @JSExport
-object App { 
+object App {
   dom.document.body.innerHTML = ""
- 
-  implicit val body = InsertionContext.append(dom.document.body)
-  implicit val executionContext = scalajs.concurrent.JSExecutionContext.queue
 
-  val test = 
-    <xml a="4">
-      <a>
-      </a>
-      <b>
-      </b>
-    </xml>
+  def view = XML.include(HTML5,"backstage.html") {
+    def user = "Martin"
+  }
+
+  dom.document.body.appendChild(view)
 }
