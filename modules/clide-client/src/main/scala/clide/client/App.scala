ff6a8040815d6e6a426d27f2cc319e7bd44f0c39
Martin Ring <mail@martinring.de>
2014-04-02 18:13:52 +0200

    Add todo example

--- modules/clide-client/src/main/scala/clide/client/App.scala
+++ modules/clide-client/src/main/scala/clide/client/App.scala
@@ -1,39 +1,38 @@
 package clide.client
 
-import clide.reactive.ui.html.HTML5
-import scala.scalajs.js.annotation.JSExport
-import clide.reactive.Event
 import scala.concurrent.duration._
-import scala.language.postfixOps
-import scala.language.reflectiveCalls
-import scala.language.existentials
-import org.scalajs.dom
-import clide.xml._
-import clide.reactive.ui.InsertionContext
 import scala.collection.mutable.ArrayBuffer
-import clide.reactive.ObservableBuffer
+import clide.xml._
+import clide.reactive._
+import clide.reactive.ui._
+import scalajs.js.annotation.JSExport
+import org.scalajs.dom
 
-@JSExport  
-object App {    
-  implicit val executionContext = scalajs.concurrent.JSExecutionContext.queue
-  implicit val scheduler = clide.reactive.ui.UiScheduler
-  
+@JSExport
+object App extends UiApp {
   dom.document.body.innerHTML = ""
      
   def basics =
     XML.include(HTML5,"basics.html")  
     
   def todos = {
-    case class Todo(text: String, done: Boolean)
-    val todos = ObservableBuffer(Todo("learn angular", true),
-                                 Todo("build an angular app", false))
-    def remaining = todos.count(!_.done)
-    def archive() = todos.foreach(todo => if (todo.done) todos -= todo)
-    def update(todo: Todo, donen: Boolean) = {
-      println("Hallo")
-      todos(todos.indexOf(todo)) = todo.copy(done = donen)
+    lazy val todos = ObservableBuffer(Todo("learn scala.js", true),
+                                     Todo("build a scala.js app", false))
+
+    case class Todo(val text: String, done: Boolean)
+    
+    // allow todos to be mutated
+    implicit class MutableTodo(todo: Todo) {
+      def done_=(value: Boolean) = 
+        todos.update(todos.indexOf(todo), todo.copy(done = value))
     }
-    def addTodo(text: String) = todos += Todo(text,false)
+     
+    def remaining = todos.count(!_.done)
+    
+    def archive() = todos.replaceAll(todos.filter(!_.done))
+    
+    def addTodo(text: String) = todos += new Todo(text,false)
+    
     XML.include(HTML5,"todo.html") 
   }
   
