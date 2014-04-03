33541d7cf81cd468ac61fcd75fb856742a1b384a
Martin Ring <mail@martinring.de>
2014-04-03 19:28:06 +0200

    update example

--- modules/clide-client/src/main/scala/clide/client/App.scala
+++ modules/clide-client/src/main/scala/clide/client/App.scala
@@ -7,35 +7,30 @@ import clide.reactive._
 import clide.reactive.ui._
 import scalajs.js.annotation.JSExport
 import org.scalajs.dom
-
+  
 @JSExport
 object App extends UiApp {
-  dom.document.body.innerHTML = ""
+  dom.document.body.innerHTML = ""     
      
-  def basics =
-    XML.include(HTML5,"basics.html")  
+  def basics = {
+    val name = Var("World")
+    XML.include(HTML5,"basics.html")
+  }
     
   def todos = {
-    lazy val todos = ObservableBuffer(Todo("learn scala.js", true),
-                                     Todo("build a scala.js app", false))
-
-    case class Todo(val text: String, done: Boolean)
-    
-    // allow todos to be mutated
-    implicit class MutableTodo(todo: Todo) {
-      def done_=(value: Boolean) = 
-        todos.update(todos.indexOf(todo), todo.copy(done = value))
-    }
-     
-    def remaining = todos.count(!_.done)
-    
+    class Todo(val text: String, val done: Boolean)
+    lazy val todos = ObservableBuffer(
+      new Todo("learn scala.js", true), 
+      new Todo("build a scala.js app", false))
+    def remaining = todos.changes.sample(todos.count(!_.done).toString)
+    def total = todos.watch.length.map(_.toString)
     def archive() = todos.replaceAll(todos.filter(!_.done))
-    
+    def setDone(todo: Todo)(value: Boolean) =
+      todos(todos.indexOf(todo)) = new Todo(todo.text, value)
     def addTodo(text: String) = todos += new Todo(text,false)
-    
-    XML.include(HTML5,"todo.html") 
+    XML.include(HTML5,"todo.html")
   }
   
-  dom.document.body.appendChild(basics)  
+  dom.document.body.appendChild(basics)
   dom.document.body.appendChild(todos)
 }
